0b21a3d2ef7f07bc62436b456875bdb8c1793434
Brian Olson <bolson@docutap.us>
2016-09-24 14:06:34 -0500

    first commit - includes provided example

---
+++ injectionBlocker.js
@@ -0,0 +1,20 @@
+alert('this is a test')
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        var blockRequest = false;
+        var xssRegex = /script/i
+        
+        if(xssRegex.test(details.url)) {
+            blockRequest = true;            
+        }
+        
+        if(blockRequest) {
+            alert('You have been protected from a potential XSS attack')
+        }
+        
+        return {cancel: blockRequest};
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestBody"]
+);
